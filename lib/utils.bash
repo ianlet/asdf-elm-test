dd0165d18d70b4a7e4981cffd41cb81afa9f72d0
Ian Letourneau <letourneau.ian@gmail.com>
2023-02-25 13:55:40 -0500

    Filter unwanted versions

--- lib/utils.bash
+++ lib/utils.bash
@@ -20,13 +20,19 @@ fi
 
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    LC_ALL=C sort -V | awk '{print $2}'
 }
 
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//' |
+    sed 's/^3\.0\.1$//' |
+    sed 's/^3\.0\.0$//' |
+    sed 's/^2\.0\.1$//' |
+    sed 's/^2\.0\.0$//' |
+    sed 's/^1\.0\.0$//' |
+    sed 's/^0\.19\.1$//'
 }
 
 list_all_versions() {
